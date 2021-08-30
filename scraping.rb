c608ec53e95b68d046744d35854338b302885c53
Taro Moirta <morita0079@gmail.com>
2021-08-30 23:20:12 +0900

    add some scraping data

--- scraping.rb
+++ scraping.rb
@@ -1,14 +1,39 @@
-require 'nokogiri'
-require 'open-uri'
+require "nokogiri"
+require "open-uri"
 
 class Scraping
   attr_reader :doc
-  def initialize(url)
-    @doc = Nokogiri::HTML(URI.open(url), nil, 'utf-8')
-  end
-end
 
-class ProperPage
+  def initialize(url)
+    @doc = Nokogiri::HTML(URI.open(url), nil, "utf-8")
+  end
+
+  def rentfee
+    xpath = "/html/body/div[1]/div[2]/form[1]/div[6]/div/div/table/tbody/tr[1]/td/div[2]/span[1]/span[1]"
+    return doc.xpath(xpath).text.delete(",").to_i
+  end
+
+  def managementfee
+    xpath = "/html/body/div[1]/div[2]/form[1]/div[6]/div/div/table/tbody/tr[1]/td/div[2]/span[1]/span[3]"
+    return doc.xpath(xpath).text.delete(",").delete("¥").to_i
+  end
+  
+  def sikikin
+    xpath = '/html/body/div[1]/div[2]/form[1]/div[6]/div/div/table/tbody/tr[1]/td/div[2]/span[2]/span[2]'
+    return huyou_to_zero(doc.xpath(xpath).text)
+  end
+  
+  def hosyokin
+    xpath = '/html/body/div[1]/div[2]/form[1]/div[6]/div/div/table/tbody/tr[1]/td/div[2]/span[2]/span[3]'
+    return huyou_to_zero(doc.xpath(xpath).text)
+  end
+
+  private
+
+  # 不要を0に変換
+  def huyou_to_zero(data)
+    return 0 if data.include?('不要')
+  end
+
 end
-t = Scraping.new('https://www.leopalace21.com/m/sp/r/detail/0000024110103/#pagetop')
-puts t.doc
+  
---
+++ test_scraping.rb
@@ -0,0 +1,24 @@
+require "test/unit"
+require "./scraping"
+
+class TC_Scraping < Test::Unit::TestCase
+  def setup
+    @obj = Scraping.new("https://www.leopalace21.com/app/searchCondition/detail/r/0000041623105.html#pagetop")
+  end
+
+  def test_get_rentfee
+    assert_equal(40000, @obj.rentfee)
+  end
+
+  def test_get_managementfee
+    assert_equal(6500, @obj.managementfee)
+  end
+  
+  def test_sikikin
+    assert_equal(0, @obj.sikikin)
+  end
+
+  def test_hosyokin
+    assert_equal(0, @obj.hosyokin)
+  end
+end
