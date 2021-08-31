b374fd895b93deeb15701aa8232e38f77803f105
Taro Morita <morita0079@gmail.com>
2021-08-31 14:51:39 +0900

    add scaraping properties struct

--- scraping.rb
+++ scraping.rb
@@ -12,6 +12,8 @@ class Scraping
     :gratuity_fee,
     :expense_deposits_fee,
     :madori_and_area,
+    :address,
+    :renewal_fee,
     keyword_init: true
   )
 
@@ -25,44 +27,96 @@ class Scraping
       gratuity_fee: '/html/body/div[1]/div[2]/form[1]/div[6]/div/div/table/tbody/tr[1]/td/div[2]/span[2]/span[5]',
       expense_deposits_fee: '/html/body/div[1]/div[2]/form[1]/div[6]/div/div/table/tbody/tr[1]/td/div[2]/span[2]/span[7]',
       madori_and_area: '/html/body/div[1]/div[2]/form[1]/div[6]/div/div/table/tbody/tr[2]/td[1]',
-      address: '/html/body/div[1]/div[2]/form[1]/div[6]/div/div/table/tbody/tr[4]/td'
+      address: '/html/body/div[1]/div[2]/form[1]/div[6]/div/div/table/tbody/tr[4]/td',
+      renewal_fee: '/html/body/div[1]/div[2]/form[1]/div[8]/div/table/tbody/tr[5]/td[1]'
     )
   end
 
-  def rentfee
-    doc.xpath(xpaths[:rentfee]).text.delete(',').to_i
-  end
-
-  def managementfee
-    doc.xpath(xpaths[:managementfee]).text.delete(',').delete('¥').to_i
-  end
-
-  def sikikin
-    # TODO 処理がおおすぎる。どうにかして切りわけたい。
-    huyou_to_zero(doc.xpath(xpaths[:sikikin]).text)
-  end
-
-  def hosyokin
-    huyou_to_zero(doc.xpath(xpaths[:hosyokin]).text)
-  end
-
-  def gratuityfee
-    doc.xpath(xpaths[:gratuity_fee]).text
-  end
-
-  def expensedepositefee
-    doc.xpath(xpaths[:expense_deposits_fee]).text
-  end
-
-  def madori_and_area
-    doc.xpath(xpaths[:madori_and_area]).text
-  end
-
-  def address
-    doc.xpath(xpaths[:address]).text
+  def allproperties
+    Properties.new(
+      rentfee: rentfee,
+      managementfee: managementfee,
+      sikikin: sikikin,
+      hosyokin: hosyokin,
+      gratuity_fee: gratuityfee,
+      expense_deposits_fee: expensedepositefee,
+      madori_and_area: madori_and_area,
+      address: address,
+      renewal_fee: renewal_fee
+    )
   end
 
   private
+
+  def scraping_data(target)
+    doc.xpath(target).text
+  end
+
+  def rentfee
+    # doc.xpath(xpaths[:rentfee]).text.delete(',').to_i
+    scraping_data(xpaths[:rentfee])
+  end
+
+  def managementfee
+    # doc.xpath(xpaths[:managementfee]).text.delete(',').delete('¥').to_i
+    scraping_data(xpaths[:managementfee])
+  end
+
+  def sikikin
+    # TODO: 処理がおおすぎる。どうにかして切りわけたい。
+    # huyou_to_zero(doc.xpath(xpaths[:sikikin]).text)
+    scraping_data(xpaths[:sikikin])
+  end
+
+  def hosyokin
+    # huyou_to_zero(doc.xpath(xpaths[:hosyokin]).text)
+    scraping_data(xpaths[:hosyokin])
+  end
+
+  def gratuityfee
+    scraping_data(xpaths[:gratuity_fee])
+  end
+
+  def expensedepositefee
+    scraping_data(xpaths[:expense_deposits_fee])
+  end
+
+  def madori_and_area
+    scraping_data(xpaths[:madori_and_area])
+  end
+
+  def address
+    scraping_data(xpaths[:address])
+  end
+
+  def renewal_fee
+    scraping_data(xpaths[:renewal_fee])
+  end
+end
+
+class TextFormatting
+  attr_reader :data
+
+  Properties = Struct.new(
+    :rentfee,
+    :managementfee,
+    :sikikin,
+    :hosyokin,
+    :gratuity_fee,
+    :expense_deposits_fee,
+    :madori_and_area,
+    :address,
+    :renewal_fee,
+    keyword_init: true
+  )
+
+  def initialize(data)
+    @data = data
+  end
+
+  private
+
+
 
   # 不要を0に変換
   def huyou_to_zero(data)
--- test_scraping.rb
+++ test_scraping.rb
@@ -6,31 +6,7 @@ class TC_Scraping < Test::Unit::TestCase
     @obj = Scraping.new("https://www.leopalace21.com/app/searchCondition/detail/r/0000041623105.html#pagetop")
   end
 
-  def test_get_rentfee
-    assert_equal(40000, @obj.rentfee)
-  end
-
-  def test_get_managementfee
-    assert_equal(6500, @obj.managementfee)
-  end
-  
-  def test_sikikin
-    assert_equal(0, @obj.sikikin)
-  end
-
-  def test_hosyokin
-    assert_equal(0, @obj.hosyokin)
-  end
-
-  def test_gratuityfee
-    p @obj.gratuityfee
-  end
-
-  def test_expensedeposite
-    p @obj.expensedepositefee
-  end
-
-  def test_address
-    p @obj.address
+  def test_all_properties
+    p @obj.allproperties
   end
 end
