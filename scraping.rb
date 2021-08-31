f756bb175bce6857b06f64c9b39517c080075c93
Taro Morita <morita0079@gmail.com>
2021-08-31 13:23:45 +0900

    add struct

--- scraping.rb
+++ scraping.rb
@@ -1,31 +1,41 @@
-require "nokogiri"
-require "open-uri"
+require 'nokogiri'
+require 'open-uri'
 
 class Scraping
-  attr_reader :doc
+  attr_reader :doc, :xpaths
+
+  Properties = Struct.new(
+    :rentfee,
+    :managementfee,
+    :sikikin,
+    :hosyokin,
+    keyword_init: true
+  )
 
   def initialize(url)
-    @doc = Nokogiri::HTML(URI.open(url), nil, "utf-8")
+    @doc = Nokogiri::HTML(URI.open(url), nil, 'utf-8')
+    @xpaths = Properties.new(
+      rentfee: '/html/body/div[1]/div[2]/form[1]/div[6]/div/div/table/tbody/tr[1]/td/div[2]/span[1]/span[1]',
+      managementfee: '/html/body/div[1]/div[2]/form[1]/div[6]/div/div/table/tbody/tr[1]/td/div[2]/span[1]/span[3]',
+      sikikin: '/html/body/div[1]/div[2]/form[1]/div[6]/div/div/table/tbody/tr[1]/td/div[2]/span[2]/span[2]',
+      hosyokin: '/html/body/div[1]/div[2]/form[1]/div[6]/div/div/table/tbody/tr[1]/td/div[2]/span[2]/span[3]'
+    )
   end
 
   def rentfee
-    xpath = "/html/body/div[1]/div[2]/form[1]/div[6]/div/div/table/tbody/tr[1]/td/div[2]/span[1]/span[1]"
-    return doc.xpath(xpath).text.delete(",").to_i
+    doc.xpath(xpaths[:rentfee]).text.delete(',').to_i
   end
 
   def managementfee
-    xpath = "/html/body/div[1]/div[2]/form[1]/div[6]/div/div/table/tbody/tr[1]/td/div[2]/span[1]/span[3]"
-    return doc.xpath(xpath).text.delete(",").delete("¥").to_i
+    doc.xpath(xpaths[:managementfee]).text.delete(',').delete('¥').to_i
   end
-  
+
   def sikikin
-    xpath = '/html/body/div[1]/div[2]/form[1]/div[6]/div/div/table/tbody/tr[1]/td/div[2]/span[2]/span[2]'
-    return huyou_to_zero(doc.xpath(xpath).text)
+    huyou_to_zero(doc.xpath(xpaths[:sikikin]).text)
   end
-  
+
   def hosyokin
-    xpath = '/html/body/div[1]/div[2]/form[1]/div[6]/div/div/table/tbody/tr[1]/td/div[2]/span[2]/span[3]'
-    return huyou_to_zero(doc.xpath(xpath).text)
+    huyou_to_zero(doc.xpath(xpaths[:hosyokin]).text)
   end
 
   private
@@ -34,6 +44,4 @@ class Scraping
   def huyou_to_zero(data)
     return 0 if data.include?('不要')
   end
-
 end
-  
